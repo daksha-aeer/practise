99353503d0d1f3f8a7aa8ae3e356f07357397826
Daksha <dakshaaeer@gmail.com>
2025-11-01 13:29:02 +0530

    update goal storage

--- dashboard/script.js
+++ dashboard/script.js
@@ -6,3 +6,31 @@ function displayDate(){
 }
 
 let timerId = setInterval(displayDate, 1000);
+
+
+let submitGoal = document.getElementById("submit-goal");
+let setGoal = document.getElementById("goal-value")
+let existingGoal = localStorage.getItem("goal")
+
+document.getElementById("goal").addEventListener("keydown", function (e){
+    if (e.key == "Enter") {
+        storeGoal();
+    }
+})
+
+submitGoal.addEventListener("click", storeGoal);
+
+function storeGoal(){
+    goalValue = document.getElementById("goal").value;
+    setGoal.textContent = "Today's focus: "+ goalValue;
+    localStorage.setItem("goal", goalValue)
+}
+
+if (existingGoal) {
+    console.log("existing goal: " + existingGoal)
+    setGoal.textContent = "Today's focus: "+ existingGoal;
+    const reset = document.createElement("button")
+}
+else {
+
+}
